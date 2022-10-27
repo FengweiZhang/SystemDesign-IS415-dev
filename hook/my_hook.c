a4c3af686abc8c9996448e3331a809c498903bfe
zhangfengwei-is415 <zhang-fengwei@foxmail.com>
2022-10-27 01:13:41 -0700

    try yi xia

--- hook/my_hook.c
+++ hook/my_hook.c
@@ -38,6 +38,7 @@ static int test_hook_init(void)
     // sys_call_ptr = get_sys_call_table();
     // printk("sys_call_ptr: 0x%lu\n", *sys_call_ptr);
     printk("Found at 0x%px \n", get_sys_call_table());
+    // try yi xia
     
 
 
