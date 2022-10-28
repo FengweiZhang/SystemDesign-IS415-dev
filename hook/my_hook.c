2cd4bd3f97bf235e9fd67e0ecb951f5750df333c
zhangfengwei-is415 <zhang-fengwei@foxmail.com>
2022-10-27 20:34:47 -0700

    try rc0 disable write protection

--- hook/my_hook.c
+++ hook/my_hook.c
@@ -7,9 +7,12 @@ MODULE_VERSION("0.1");
 
 // End: Kernel Module
 
+#include <linux/init.h>
 #include <linux/types.h>
 #include <linux/kernel.h>
 #include <linux/kprobes.h>
+#include <linux/syscalls.h>
+#include <asm/unistd_64.h>
 
 
 #include "../common/prm_error.h"
@@ -25,8 +28,6 @@ static struct kprobe kp = {
     .symbol_name = "kallsyms_lookup_name"
 };
 
-unsigned long cr0 = 0;
-
 /**
  * @brief Get the sys call table position
  * 
@@ -41,6 +42,22 @@ static sys_call_ptr_t* get_sys_call_table(void)
     return (sys_call_ptr_t*)kp.addr;
 }
 
+typedef asmlinkage long (*sys_open_t)(const char __user *filename, umode_t mode);
+
+sys_open_t real_chmod;
+
+asmlinkage long my_sys_chmod(const char __user *filename, umode_t mode)
+{
+    printk("Hook success");
+    return real_chmod(filename, mode);
+}
+
+extern unsigned long __force_order ;
+inline void write_cr0_new(unsigned long cr0) 
+{
+    __asm__ volatile("mov %0,%%cr0" : "+r"(cr0), "+m"(__force_order));
+}
+
 
 static int test_hook_init(void)
 {
@@ -49,8 +66,17 @@ static int test_hook_init(void)
     sys_call_ptr = get_sys_call_table();
     // printk("sys_call_table found at 0x%px \n", sys_call_ptr);
 
-    
+    // real_chmod = (void *)sys_call_ptr[__NR_chmod];
+    // sys_call_ptr[__NR_chmod] = (sys_call_ptr_t)my_sys_chmod;
 
+    unsigned long cr0 = read_cr0();
+    printk("CR0: %016lx", cr0);
+
+    write_cr0_new(read_cr0() & (~0x10000));
+    printk("disable: %016lx", read_cr0());
+
+    write_cr0_new(read_cr0() | (0x10000));
+    printk("enable: %016lx", read_cr0());
 
     return 0;
 }
@@ -58,6 +84,9 @@ static int test_hook_init(void)
 
 static void test_hook_exit(void)
 {
+
+    // sys_call_ptr[__NR_chmod] = (sys_call_ptr_t)real_chmod;
+
     unregister_kprobe(&kp);
 }   
 
