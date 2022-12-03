0f4a0385fa53a28356fb31f8f0c1205c76c4038a
Fengwei Zhang, IS415 <zhang-fengwei@foxmail.com>
2022-12-03 00:43:47 -0800

    hook execve debug

--- src/kernel/prm_hook.c
+++ src/kernel/prm_hook.c
@@ -287,7 +287,7 @@ asmlinkage long my_sys_socket(struct pt_regs *regs)
     int check_ret = PRM_ERROR;
 
     uid = current_uid().val;
-    printk("socket create %u\n", uid);
+    // printk("socket create %u\n", uid);
     check_ret = check_privilege(0, uid, P_NET, &p_result);
     if(check_ret != PRM_SUCCESS)
     {
@@ -314,7 +314,7 @@ asmlinkage long my_sys_socket(struct pt_regs *regs)
  * @param regs 
  * @return asmlinkage 
  */
-asmlinkage long sys_execve(struct pt_regs *regs)
+asmlinkage long my_sys_execve(struct pt_regs *regs)
 {
     long ret = -1;
     uid_t uid;
@@ -351,12 +351,14 @@ int prm_hook_init(void)
     // real_write =    (void *)sys_call_ptr[__NR_write];
     real_reboot =   (void *)sys_call_ptr[__NR_reboot];
     real_socket =   (void *)sys_call_ptr[__NR_socket];
+    real_execve =   (void *)sys_call_ptr[__NR_execve];
 
     // 修改系统调用表
     // sys_call_ptr[__NR_read] =       (sys_call_ptr_t)my_sys_read;
     // sys_call_ptr[__NR_write] =      (sys_call_ptr_t)my_sys_write;
     sys_call_ptr[__NR_reboot] =     (sys_call_ptr_t)my_sys_reboot;
     sys_call_ptr[__NR_socket] =     (sys_call_ptr_t)my_sys_socket;
+    sys_call_ptr[__NR_execve] =     (sys_call_ptr_t)my_sys_execve;
 
     write_protection_on();
     printk("%s %s: System calls hook set.\n", module_name, name);
@@ -379,6 +381,7 @@ int prm_hook_exit(void)
     // sys_call_ptr[__NR_write] =      (sys_call_ptr_t)real_write;
     sys_call_ptr[__NR_reboot] =     (sys_call_ptr_t)real_reboot;
     sys_call_ptr[__NR_socket] =     (sys_call_ptr_t)real_socket;
+    sys_call_ptr[__NR_execve] =     (sys_call_ptr_t)real_execve;
 
     write_protection_on();
     printk("%s %s: System calls hook unset.\n", module_name, name);
