d187da6212b0866dbac4ade6c98d88f728eff2cf
Fengwei Zhang, IS415 <zhang-fengwei@foxmail.com>
2022-12-03 22:47:10 -0800

    reg read 测试程序

--- src/test/test_read.c
+++ src/test/test_read.c
@@ -3,15 +3,15 @@
 
 
 int main(){
-    char buf[10] = "ttt\n";
+    char buf[10000];
     char filename[100] = "/home/ubuntu/Workspace/123";
-    FILE *pf = fopen(filename, "a");
-    if (pf == NULL)
+    FILE *fp = fopen(filename, "r");
+    if (fp == NULL)
     {
         // perror("open file for reading");
         exit(0);
     }
-    fputs(buf, pf);
-    perror("write failed");
-    fclose(pf);
+    fscanf(fp, "%s", buf);
+    perror("read");
+    fclose(fp);
 }
